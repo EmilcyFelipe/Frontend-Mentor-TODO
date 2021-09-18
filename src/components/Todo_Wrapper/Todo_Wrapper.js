3dba178deab0aa7904303e39dbb093c7d8f09d9a
Felipe de Paula <emilcyfelipe@gmail.com>
2021-09-17 23:01:01 -0300

    Responsived

--- src/components/Todo_Wrapper/Todo_Wrapper.js
+++ src/components/Todo_Wrapper/Todo_Wrapper.js
@@ -64,7 +64,7 @@ export default function TodoWrapper(){
             </form>
             </div>
             <ListComponent list={taskList} func={setTaskList}/>
-
+            <div className="signature">Made by Felipe de Paula</div>
         </div>
     )
 }
